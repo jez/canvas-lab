ea55acdd09c9893ceebaa4e6133ef7ba8e528e72
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-06-13 23:59:55 -0400

    Define some useful variables
    Notice that in this demo we will be using the document's querySelector,
    just to introduce a different syntax for the same thing. Since this part
    of the demo isn't really part of the lab, the code will be provided.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 ;(function() {
-
+  // Define some useful variables that we will need
+  var   canvas = document.querySelector('canvas'),
+         ctx = canvas.getContext('2d'),
+   particles = [],
+patriclesNum = 500,
+           w = 500,
+           h = 500,
+      colors = ['#f35d4f','#f36849','#c0d988','#6ddaf1','#f1e85b'];
 })();
